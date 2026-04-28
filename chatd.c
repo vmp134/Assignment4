986919eefc9928923a76f21ac5266be3c1248e0b
Victor <vmp134@scarletmail.rutgers.edu>
2026-04-28 12:36:47 -0400

    Updated chatd.c, chatd.h

--- chatd.c
+++ chatd.c
@@ -1,10 +1,20 @@
 #include "chatd.h"
 
 int main(int argc, char **argv) {
+    
+    //Setup
+    if (argc != 2) {
+        fprintf(stderr, "Error: Incorrect # of Arguments\n");
+        return EXIT_FAILURE;
+    }
+    int port = atoi(argv[1]);
+    int server_fd;
 
-
-
-
+    if (server_fd = socket(AF_INET, SOCK_STREAM, 0) < 0) {
+        perror("Socket");
+        return EXIT_FAILURE;
+    }
+    
 
     return EXIT_SUCCESS;
 }
--- chatd.h
+++ chatd.h
@@ -1,3 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define NAME_MAX 32
+#define STATUS_MAX 64
+#define MESSAGE_MAX 80
+
+typedef enum {
+    NAM,    
+    SET,
+    MSG,
+    WHO,
+    ERR
+} MessageType;
